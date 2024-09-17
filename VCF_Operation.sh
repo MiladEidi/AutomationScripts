932a322268012e76d559452d95f4cd1f669b6aa1
MiladEidi <127682697+MiladEidi@users.noreply.github.com>
2024-09-17 12:02:29 +0330

    VCF_Operation description
    It does some important steps on VCF files that you need to do after variant calling. 
    Makes variants Biallelic, left normalized, filter out off-target variants, annotation using ANNOVAR, and modifying couple of misleading annotations which ANNOVAR added...
    Filtration of variants based on the clinical and biologic annotations can be added at the end of the script or can be done by 123VCF!

--- VCF_Operation.sh
+++ VCF_Operation.sh
@@ -1,21 +1,51 @@
 #!/bin/bash
 set -euo pipefail
 
+# Function to display usage information
+usage() {
+  echo "Usage: $0 <vcfFile> <outputPrefix> <refFASTA> <bed> <annovarFolder> <annovardbFolder> <refBuild> [protocol] [operation]"
+  echo "  vcfFile:         Input VCF file"
+  echo "  outputPrefix:    Prefix for output files"
+  echo "  refFASTA:        Reference FASTA file"
+  echo "  bed:             BED file for filtering off-target variants"
+  echo "  annovarFolder:   Path to ANNOVAR scripts"
+  echo "  annovardbFolder: Path to ANNOVAR databases"
+  echo "  refBuild:        Reference build (e.g., hg19 or hg38)"
+  echo "  Threads:         ANNOVAR threads"
+  echo "  protocol:        (Optional) ANNOVAR protocol string"
+  echo "  operation:       (Optional) ANNOVAR operation string"
+  exit 1
+}
+
+# Check if enough arguments are provided
+if [[ $# -lt 8 ]]; then
+  echo "Error: Missing arguments."
+  usage
+fi
+
 # Variables setup
 vcfFile=$1           # Input VCF file
 outputPrefix=$2      # Output file prefix
 refFASTA=$3          # Reference FASTA file
-annovarFolder=$4     # ANNOVAR folder path
-annovardbFolder=$5   # ANNOVAR databases folder
-refBuild=$6          # Reference build (e.g., hg19 or hg38)
+bed=$4				 # BED file for hard filtering off-target variants
+annovarFolder=$5     # ANNOVAR folder path
+annovardbFolder=$6   # ANNOVAR databases folder
+refBuild=$7          # Reference build (e.g., hg19 or hg38)
+thread=$8
 
 # Optional arguments for protocol and operation (default values used if not provided)
-protocol="refGeneWithVer,ensGene,genomicSuperDups,clinvar_20240611,1000g2015aug_all,gnomad211_genome,gnomad_exome,exac03,kaviar,esp6500siv2_all,gme,hrcr1,iranome,abraom,intervar_20180118,dann,CADD16,revel,dbnsfp41c,avsnp150"
-operation="g,g,r,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f"
+# if hg19
+protocol="refGeneWithVer,knownGene,ensGene,genomicSuperDups,clinvar_20240611,1000g2015aug_all,gnomad211_genome,gnomad211_exome,exac03,kaviar,esp6500siv2_all,gme,hrcr1,iranome,abraom,intervar_20180118,dann,CADD16,revel,dbnsfp41c,avsnp150"
+operation="g,g,g,r,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f"
 
-if [[ $# -ge 8 ]]; then
-  protocol=$7
-  operation=$8
+# if hg38
+# protocol="refGeneWithVer,knownGene,ensGene,genomicSuperDups,clinvar_20240611,1000g2015aug_all,gnomad41_exome,gnomad41_genome,kaviar,esp6500siv2_all,gme,hrcr1,iranome,abraom,intervar_20180118,dann,CADD16,revel,dbnsfp41c,avsnp150"
+# operation="g,g,g,r,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f,f"
+
+
+if [[ $# -ge 10 ]]; then
+  protocol=$9
+  operation=$10
 fi
 
 # Create output directory if it doesn't exist
@@ -27,39 +57,63 @@ command -v tabix >/dev/null 2>&1 || { echo "Tabix not found. Exiting."; exit 1; 
 command -v "${annovarFolder}/table_annovar.pl" >/dev/null 2>&1 || { echo "ANNOVAR script not found. Exiting."; exit 1; }
 
 main() {
-  ## Step 1: Split multiallelic variants into biallelic, perform left normalization,
-  filteredVcf="${outputPrefix}.biallelic.leftnorm.vcf.gz"
-  bcftools norm -m-both -f "${refFASTA}" -Oz -o "${filteredVcf}" "${vcfFile}"
+  ## Split multiallelic variants into biallelic, perform left normalization, and doing hard filter for off-target variants
+  filteredVcf="${outputPrefix}.biallelic.leftnorm0.vcf.gz"
+  bcftools norm -Oz -o "${filteredVcf}" -m-both -f "${refFASTA}" "${vcfFile}"
+  
+  tabix -p vcf -f "${filteredVcf}"
+  
+  bcftools filter -R "${bed}" -Oz -o "${outputPrefix}.biallelic.leftnorm.vcf.gz" "${filteredVcf}"
 
   # Index the filtered VCF file
+  filteredVcf="${outputPrefix}.biallelic.leftnorm.vcf.gz"
   tabix -p vcf "${filteredVcf}"
 
-  ## Step 2: Annotate the VCF file with ANNOVAR
+  ## Annotate the VCF file with ANNOVAR
   vcfPlain="${outputPrefix}.unzipped.vcf"
-
-  # Prepare VCF without genotypes and annotate using ANNOVAR
+  # Prepare VCF without genotypes and couple of info annotations to make the annotation faster as the input file would be smaller
   bcftools view -G -Ou "${filteredVcf}" | bcftools annotate -x FORMAT/GT,^INFO/AC -o "${vcfPlain}"
   
-  "${annovarFolder}/table_annovar.pl" "${vcfPlain}" "${annovardbFolder}" -buildver "${refBuild}" -out "${outputPrefix}.annovar" \
+  # Annotation using ANNOVAR
+  "${annovarFolder}/table_annovar.pl" -thread "${thread}" "${vcfPlain}" "${annovardbFolder}" -buildver "${refBuild}" -out "${outputPrefix}.annovar" \
   -remove -protocol "${protocol}" -operation "${operation}" \
   -nastring . -vcfinput > "${outputPrefix}.annovar.log"
+  
+  bgzip "${outputPrefix}.annovar.${refBuild}_multianno.vcf"
+  tabix -p vcf "${outputPrefix}.annovar.${refBuild}_multianno.vcf.gz"
+  
+  # Now let's add the eliminated info to the annotated VCF file again
+  vcfAnnotated="${outputPrefix}.annovar.GT.vcf"
+  bcftools annotate -a "${filteredVcf}" --force -c INFO -Ov -o ${vcfAnnotated} "${outputPrefix}.annovar.${refBuild}_multianno.vcf.gz"
 
-  ## Step 3: Adjust annotation types for consistency in downstream processing
+  ## Adjust annotation types for consistency in downstream processing
   sed -i -e '/^##INFO=<ID=gnomAD/s/Type=String/Type=Float/' \
          -e '/^##INFO=<ID=ExAC_nontcga/s/Type=String/Type=Float/' \
          -e '/^##INFO=<ID=CADD/s/Type=String/Type=Float/' \
          -e '/^##INFO=<ID=REVEL/s/Type=String/Type=Float/' \
-         -e 's/Eigen-/Eigen_/g' \
+	 -e '/^##INFO=<ID=DANN_score/s/Type=String/Type=Float/' \
+	 -e 's/Eigen-/Eigen_/g' \
          -e 's/GERP++/GERPpp/g' \
          -e 's/PC-/PC_/g' \
          -e 's/M-CAP/M_CAP/g' \
-         -e 's/fathmm-/fathmm_/g' "${outputPrefix}.annovar.${refBuild}_multianno.vcf"
+         -e 's/fathmm-/fathmm_/g' "${outputPrefix}.annovar.GT.vcf"
 
-  ## Step 4: Cleanup intermediate files
+  ## Compress the final file
+  bgzip "${outputPrefix}.annovar.GT.vcf"
+  tabix -p vcf "${outputPrefix}.annovar.GT.vcf.gz"
+  
+  ## Cleanup intermediate files
   rm -f "${vcfPlain}" \
-        "${outputPrefix}.annovar.${refBuild}_multianno.vcf" \
+  	"${outputPrefix}.biallelic.leftnorm0.vcf.gz" \
+  	"${outputPrefix}.biallelic.leftnorm.vcf.gz" \
+        "${outputPrefix}.annovar.${refBuild}_multianno.vcf.gz" \
+        "${outputPrefix}.annovar.${refBuild}_multianno.vcf.gz.tbi"
         "${outputPrefix}.annovar.${refBuild}_multianno.txt" \
         "${outputPrefix}.annovar.avinput"
+        
+        
+        
+        
 }
 
 # Call the main function
